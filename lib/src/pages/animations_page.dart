a924d34aac1c34a243fea11f1a1060486cff0cc5
MichiDevelop <michidevelop@gmail.com>
2023-01-14 18:12:15 -0600

    Animaciones: clase 28

---
+++ lib/src/pages/animations_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AnimationsPage extends StatelessWidget {
+  const AnimationsPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  const CuadradoAnimado({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CuadradoAnimado> createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 4000));
+    rotacion = Tween(begin: 0.0, end: 2.0).animate(controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  const _Rectangulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: const BoxDecoration(color: Colors.blue),
+    );
+  }
+}
