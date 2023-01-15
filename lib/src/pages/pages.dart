ca72836bc8c58e45a144b7b78112efa83f337a85
MichiDevelop <michidevelop@gmail.com>
2023-01-15 09:01:46 -0600

    ProgressBar circular: clase 48

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -1,4 +1,6 @@
-export 'package:desing_fonts/src/pages/circular_progress_page.dart';
+export 'package:desing_fonts/src/pages/graficas_circulares_page.dart';
+
+export 'package:desing_fonts/src/labs/circular_progress_page.dart';
 
 export 'package:desing_fonts/src/pages/animations_page.dart';
 
