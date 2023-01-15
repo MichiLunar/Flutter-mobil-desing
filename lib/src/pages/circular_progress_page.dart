9beba8e512ed4bd61e516f2cfe78078fe70db38c
MichiDevelop <michidevelop@gmail.com>
2023-01-14 23:09:59 -0600

    ProgressBar circular: clase 45

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
@@ -13,11 +15,54 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          padding: const EdgeInsets.all(5),
           width: 300,
           height: 300,
-          color: Colors.indigo,
+          //color: Colors.indigo,
+          child: CustomPaint(
+            painter: _MiRadialProgress(50),
+          ),
         ),
       ),
     );
   }
+}
+
+class _MiRadialProgress extends CustomPainter {
+  final porcentaje;
+
+  _MiRadialProgress(this.porcentaje);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Circulo completado de fondo
+    final paint = Paint()
+      ..strokeWidth = 4
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke;
+
+    final Offset center = Offset(size.width * 0.5, size.height * 0.5);
+    final double radio = min(size.width * 0.5, size.height * 0.5);
+    canvas.drawCircle(center, radio, paint);
+
+    // Arco
+    final paintArco = Paint()
+      ..strokeWidth = 10
+      ..color = Colors.indigo
+      ..style = PaintingStyle.stroke;
+
+    // Parte que se va llenando
+
+    double arcAngle = 2 * pi * (porcentaje / 100);
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radio),
+      -pi / 2,
+      arcAngle,
+      false,
+      paintArco,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
